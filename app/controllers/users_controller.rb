599698cb56d3364acd74f46a49b4c8b4c9c814b1
Fredrik Andersson <fredrik.andersson@aol.com>
2020-04-17 00:51:37 +0200

    Add Authentication methods, update navbar and logged in verification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
 
   def show 
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
@@ -14,11 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your profile was successfully updated!"
       redirect_to @user
@@ -35,9 +33,14 @@ class UsersController < ApplicationController
     else
       render "new"
     end
-  end
+    
+  end 
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
