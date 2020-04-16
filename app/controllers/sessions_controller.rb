b0014205204df67177767b72314bebf0f610eba7
Fredrik Andersson <fredrik.andersson@aol.com>
2020-04-16 13:14:16 +0200

    Add create and destoy actions : login session and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,24 @@ def new
 end
 
 def create
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+    flash[:notice] = "Logged in successfully!"
+    redirect_to user
+  else
+    flash.now[:alert] = "There was something wrong with your login details"
+    render "new"
+  end
+  #flash.now is used when we dont redirect to another page, is shows up "now"
   # params[:session][:email]
   # params[:session][:password]
 end
 
 def destroy
+  session[:user_id] = nil
+  flash[:notice] = "Logged out"
+  redirect_to root_path
 end
 
 end
