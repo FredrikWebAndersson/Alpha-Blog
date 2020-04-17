6b5b30d3df983343d44f3f592be63dfd8bfb6278
Fredrik Andersson <fredrik.andersson@aol.com>
2020-04-17 02:33:32 +0200

    Complete user autentication system, restrict actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 
 class ArticlesController < ApplicationController
+  # before actions allways in order ! 
   before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
@@ -18,7 +21,7 @@ class ArticlesController < ApplicationController
 
   def create 
     @article = Article.new(article_params) #Security to admit creation, or update 
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created succesfully !"
       redirect_to @article
@@ -42,6 +45,13 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def require_same_user
+    if current_user!= @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
+  end
 
   def set_article
     @article = Article.find(params[:id])
